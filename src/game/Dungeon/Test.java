a296a1b437a0961b238933b554c7ba92a47fc181
joshuabeaulieu <beaulieuj4@wit.edu>
2018-07-18 10:34:30 -0400

    Added the base for the dungeon generator using Prim's algorithm random layout works its just a empty for now still needs to be worked on

--- src/game/Dungeon/Test.java
+++ src/game/Dungeon/Test.java
@@ -2,6 +2,6 @@ package game.Dungeon;
 
 public class Test {
 	public static void main(String[] args){
-		System.out.println(new Dungeon(100,100).toString());
+		System.out.println(new Dungeon(50,50).toString());
 	}
 }
